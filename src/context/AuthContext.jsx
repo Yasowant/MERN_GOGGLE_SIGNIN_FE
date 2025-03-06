0d941f5e0a44c164f5f324d602a8c1b1e4e3e826
Yasowant <yasowant1998@gmail.com>
2025-03-06 09:30:03 +0530

    log out button issues fixed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 export const AuthContext = createContext(); // Export directly
 
+console.log(import.meta.env.VITE_API_URL, 'import.meta.env.VITE_API_URL');
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
@@ -10,14 +12,17 @@ export const AuthProvider = ({ children }) => {
     const fetchUser = async () => {
       try {
         const res = await axios.get(
-          `${import.meta.env.VITE_API_URL}/auth/profile`,
+          `${import.meta.env.VITE_API_URL}/auth/profile`, // Use VITE_API_URL
           {
             withCredentials: true,
           }
         );
         setUser(res.data);
       } catch (error) {
-        console.error('User not authenticated');
+        console.error(
+          '❌ User not authenticated:',
+          error.response?.data || error.message
+        );
         setUser(null);
       }
     };
